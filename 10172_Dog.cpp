b6b52362c2681d3aa0238ec0db4430793c2064fc
IsThereAnyCode <hjk7625@gmail.com>
2022-09-09 22:10:39 +0900

    I made Dog

---
+++ 10172_Dog.cpp
@@ -0,0 +1,22 @@
+//
+//  10172_Dog.cpp
+//  BaekJoon
+//
+//  Created by NoMore on 2022/09/09.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "|" << "\\" << "_" << "/" << "|" << endl;
+    cout << "|" << "q" << " " << "p" << "|" << "   " << "/" << "}" << endl;
+    cout << "(" << " " << "0" << ")" << " " << "\"" << "\"" << "\"" << "/" << "}" <<endl;
+    cout << "|" << "\"" << "^" << "\"" << "\'" << "    " << "|" << endl;
+    cout << "|" << "|" << "_" << "/" << "=" << "\\" << "\\" << "__" << "|" << endl;
+    
+    return 0;
+}
